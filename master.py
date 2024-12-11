83da4ad79db4418b393cad06a3328054d5b60bff
WDan1el <189869788+WDan1el@users.noreply.github.com>
2024-12-11 07:37:48 -0500

    ejercicio 2  terminado

--- master.py
+++ master.py
@@ -1,6 +1,17 @@
 n = int(input("Introduce el valor de n: "))
 
-for i in range(1, n + 1):
-    
-    if i % 3 != 0 and i % 7 != 0:
-        print(i)
+S1 = sum(range(1, n + 1))
+
+
+S2 = n * (n + 1) // 2
+
+
+print(f"S1 (con suma de los primeros n números naturales) es: {S1}")
+print(f"S2 (con la fórmula n*(n+1)/2) es: {S2}")
+
+
+if S1 == S2:
+    print("S1 y S2 son iguales.")
+else:
+    print("S1 y S2 no son iguales.")
+#em pese a aser los otors proyectos por mi cuenta#
